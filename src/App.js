9f7d55a529ba37744e5a557fceae994a4b2d10e6
SanjanaVeeranna <sanjana.v1405@gmail.com>
2023-12-09 19:52:16 +0530

    my_commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Example from './container/Example';
+import './style.css'
+
+function App() {
+  return (
+    <>
+    <Example />
+    </>
+  );
+}
+
+export default App;
---
+++ src/container/home.jsx
@@ -0,0 +1,18 @@
+const Example = () => {
+    return(
+        <>
+    <div className="container">
+      <header>
+        <h1>Welcome to My React Page</h1>
+      </header>
+      <main>
+        <p>This is a basic example of a React webpage.</p>
+      </main>
+      <footer>
+        <p>Footer content here</p>
+      </footer>
+    </div>
+        </>
+    )
+}
+export default Example;
